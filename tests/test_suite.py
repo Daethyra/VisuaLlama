1e8252d32462ca09daf2ddba3d97c9360b164d71
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-29 21:57:55 -0700

    modified:   LICENSE 	modified:   tests/test_suite.py

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -4,7 +4,10 @@ This module contains the test suite for the VisuaLlama application.
 Author:
     Daemon 'Daethyra' Carino
     
-
+LICENSE: 
+    
+TODO:
+    * Complete test suite for all modules.
 """
 import unittest
 
