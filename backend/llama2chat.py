b55639b245f1490e79666340eff0cbb1b3e32060
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-29 17:02:08 -0700

    new file:   backend/detectron2_manager.py 	modified:   backend/llama2chat.py 	modified:   backend/utility.py 	new file:   documents/Code-Reviews/8-29-23/Review-B.md

--- backend/llama2chat.py
+++ backend/llama2chat.py
@@ -1,22 +1,27 @@
 from utility import Utility
-import logging
+from transformers import pipeline
 
 class Llama2Chat:
-    """A class for generating natural language text using LLaMA 2."""
-    
-    def __init__(self, utility: Utility):
-        self.logger = utility.configure_logger(__name__)
-        self.pipeline = utility.text_generation_pipeline
-        self.logger.info("Llama2Chat initialized.")
+    def __init__(self):
+        self.utility = Utility()
+        self.text_gen = pipeline("text-generation", model=self.utility.llama2_model, tokenizer=self.utility.llama2_tokenizer)
         
-    def generate_explanation(self, label_to_count: str, count: int) -> str:
-        """Generates a natural language explanation based on the count of label_to_count."""
-        try:
-            self.logger.info("Generating explanation...")
-            prompt = f"How would you explain the presence of {count} {label_to_count}(s) in a room?"
-            explanation = self.pipeline(prompt)
-            self.logger.info("Explanation generated successfully.")
-            return explanation
-        except Exception as e:
-            self.logger.error(f"An error occurred while generating the explanation: {e}")
-            return "An error occurred while generating the explanation."
+    def generate_text(self, prompt):
+        # Use LLaMA2 model and tokenizer for text generation
+        generated_text = self.text_gen(prompt)[0]['generated_text']
+        
+        # Extract dynamic classes based on the generated text
+        dynamic_classes = self.extract_dynamic_classes(generated_text)
+        
+        return generated_text, dynamic_classes
+
+    def extract_dynamic_classes(self, generated_text):
+        # Placeholder logic to extract dynamic classes based on user input and model output
+        # This can be a complex NLP task and might require additional model training or rule-based logic
+        
+        # Example: Extracting object types and their characteristics from the generated text
+        dynamic_classes = {}  # Dictionary to hold dynamic classes
+        
+        # TODO: Implement logic to populate dynamic_classes based on generated_text
+        
+        return dynamic_classes
