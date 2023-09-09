1b7605d775b76387bccc9d6482bead567dede3fc
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-09-08 21:17:00 -0700

    modified:   orchestration/Dockerfile 	modified:   orchestration/start_services.sh

--- orchestration/start_services.sh
+++ orchestration/start_services.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Check for necessary environment variables
+if [[ -z "$NGROK_AUTH_TOKEN" || -z "$NGROK_API_KEY" ]]; then
+  echo "Required NGROK environment variables are not set. Exiting."
+  exit 1
+fi
+
 # Install PDM into an isolated environment
 curl -sSLO https://pdm.fming.dev/install-pdm.py
 curl -sSL https://pdm.fming.dev/install-pdm.py.sha256 | shasum -a 256 -c -
-# python3 install-pdm.py
+python3 install-pdm.py >/dev/null 2>&1
 
 # Install Ngrok using the specified method
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
 echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 sudo apt update
-sudo apt install ngrok -y
+sudo apt install ngrok -y >/dev/null 2>&1
 
 # Export environment variables from the .env file
 set -o allexport
@@ -17,16 +23,26 @@ source /home/VisuaLlama/.env
 set +o allexport
 
 # Configure Ngrok Auth Token
-ngrok config add-authoken $NGROK_AUTH_TOKEN
-ngrok config add-api-key $NGROK_API_KEY
+ngrok config add-authoken $NGROK_AUTH_TOKEN >/dev/null 2>&1
+ngrok config add-api-key $NGROK_API_KEY >/dev/null 2>&1
 
-# Execute PDM to install dependencies
-python3 install-pdm.py
+# Check PATH and Python/Pip
+echo "Current PATH: $PATH"
+which python3
+which pip
+
+# Install PDM
+python3 install-pdm.py >/dev/null 2>&1
+
+# Export dependencies to the PATH
+export PATH=/usr/bin:$PATH
 export PATH=/home/VisuaLlama/.local/bin:$PATH
-pdm install
+
+# Install Python packages
+pdm install >/dev/null
 
 # Start Python HTTP server in the background
-python3.8 -m http.server 8088 &
+python3.8 -m http.server 8088
 
 # Start Ngrok in the foreground
-ngrok http 8088
+ngrok http 8088 
