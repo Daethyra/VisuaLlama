c10b5d7f2ae70bf9e12e5ea398a46b23ae31fc85
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-09-08 20:41:28 -0700

    modified:   orchestration/Dockerfile 	modified:   orchestration/start_services.sh

--- orchestration/start_services.sh
+++ orchestration/start_services.sh
@@ -3,13 +3,13 @@
 # Install PDM into an isolated environment
 curl -sSLO https://pdm.fming.dev/install-pdm.py
 curl -sSL https://pdm.fming.dev/install-pdm.py.sha256 | shasum -a 256 -c -
-python3 install-pdm.py
+# python3 install-pdm.py
 
 # Install Ngrok using the specified method
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
 echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
 sudo apt update
-sudo apt install ngrok
+sudo apt install ngrok -y
 
 # Export environment variables from the .env file
 set -o allexport
@@ -21,6 +21,8 @@ ngrok config add-authoken $NGROK_AUTH_TOKEN
 ngrok config add-api-key $NGROK_API_KEY
 
 # Execute PDM to install dependencies
+python3 install-pdm.py
+export PATH=/home/VisuaLlama/.local/bin:$PATH
 pdm install
 
 # Start Python HTTP server in the background
