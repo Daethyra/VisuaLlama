8d7a9d3e19be520c16eb8eadedcf8809d1712474
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-09-09 23:34:21 -0700

    Ngrok Authtoken not being set; All other lights green

--- orchestration/start_services.sh
+++ orchestration/start_services.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-# Install PDM into an isolated environment
-curl -sSLO https://pdm.fming.dev/install-pdm.py
-curl -sSL https://pdm.fming.dev/install-pdm.py.sha256 | shasum -a 256 -c -
-python3 install-pdm.py >/dev/null 2>&1
-
-# Install Ngrok using the specified method
-curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
-echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-sudo apt update
-sudo apt install ngrok -y >/dev/null 2>&1
-
-# Configure Ngrok Auth Token
-ngrok config add-authoken $NGROK_AUTH_TOKEN >/dev/null 2>&1
-ngrok config add-api-key $NGROK_API_KEY >/dev/null 2>&1
-
-# Check PATH and Python/Pip
-echo "Current PATH: $PATH"
-which python3
-which pip
-
-# Install PDM
-python3 install-pdm.py >/dev/null 2>&1
-
 # Export dependencies to the PATH
 export PATH=/usr/bin:$PATH
 export PATH=/home/VisuaLlama/.local/bin:$PATH
 
+# Check PATH and Python/Pip
+echo "Current PATH: $PATH" | tee -a /home/VisuaLlama/pysetup.log
+which python3 | tee -a /home/VisuaLlama/pysetup.log
+which pip | tee -a /home/VisuaLlama/pysetup.log
+
+# Install PDM
+curl -sSLO https://pdm.fming.dev/install-pdm.py
+curl -sSL https://pdm.fming.dev/install-pdm.py.sha256 | shasum -a 256 -c -
+python3 install-pdm.py >/dev/null
+
+# Install Ngrok using the specified method
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+sudo apt update && sudo apt install ngrok -y
+
+sleep 1
+# Configure Ngrok Tokens
+ngrok config add-authoken $NGROK_AUTHTOKEN && ngrok config add-api-key $NGROK_API_KEY
+
 # Install Python packages
-pdm install >/dev/null
+pdm install
 
 # Start Python HTTP server in the background
 python3.8 -m http.server 8088
