9cd46430d1a14cf1487117dbba09a9f13f6fe03d
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-09-11 20:51:01 -0700

    Changed the module structure to properly implement Meta's modules 	renamed:    .github/workflows/dependency-tests.yml -> .github/workflows/Orchestration-Tests.yml 	modified:   .gitignore 	modified:   CITATION.cff 	renamed:    src/LLMhandlers/vilt_power.py -> src/LLMhandlers/dynamic_vqa.py 	deleted:    src/LLMhandlers/llama_loader.py 	renamed:    src/LLMhandlers/meta/docs/README.md -> src/LLMhandlers/meta/README.md 	new file:   src/LLMhandlers/multi_model.py

--- src/LLMhandlers/dynamic_vqa.py
+++ src/LLMhandlers/dynamic_vqa.py
@@ -1,4 +1,4 @@
-
+" Supports loading of VILT model and dynamic prediction of images "
 import asyncio
 import tensorflow as tf
 from transformers import AutoModel, AutoTokenizer
@@ -6,7 +6,7 @@ from PIL import Image
 import numpy as np
 from meta.generation import Llama # Import the generation class from LLaMA-2-13b-chat
 
-class EnhancedVILTChatbotModule:
+class VILTChatbot:
     def __init__(self, llama_model, model_name="dandelin/vilt-b32-finetuned-vqa"):
         self.processor = AutoTokenizer.from_pretrained(model_name)
         self.model = AutoModel.from_pretrained(model_name)
@@ -39,22 +39,7 @@ class EnhancedVILTChatbotModule:
         return np.array(batch_features)
 
     def extract_features(self, batch_features):
-        # Quantify or transform features as needed
-        # For demonstration, assuming features are already in a usable format
+        # Add logic to extract features from batch_features
+        # and return a list of features that Llama can interpret easily
+
         return batch_features
-
-# Example usage
-async def main():
-    # Initialize the Tokenizer and Llama instances (replace with your actual instances)
-    tokenizer = Tokenizer('path/to/your/tokenizer/model')
-    llama_instance = Llama('path/to/your/llama/model', tokenizer)
-    
-    # Initialize the EnhancedVILTChatbotModule
-    module = EnhancedVILTChatbotModule(llama_instance)
-    
-    # Run a prediction
-    result = await module.dynamic_predict("a cat", ["image1.jpg", "image2.jpg"])
-    print(result)
-
-# Uncomment to run the example
-# asyncio.run(main())
